bc29b489f1910e4bcbab21f5b5cff9825e2a23ae
Your Name <you@example.com>
2022-03-07 15:48:55 +0300

    task1
    fix3

--- ex01.hpp
+++ ex01.hpp
@@ -3,7 +3,8 @@
 //  the function below now only works with integers, change it to work with any
 //  type also fill out the body
 template <typename T> void multiples(T &sum, T x, int n) {
-  for (int i = 0; i < (int)n; i++) {
+  sum = 1;
+  for (int i = 0; i <= (int)n; i++) {
     sum += x * n;
   }
 }
